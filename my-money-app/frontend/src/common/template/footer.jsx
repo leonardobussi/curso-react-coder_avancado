4b7969d1dbeaf0e09f2983226e11ff27c947ffe2
leonardobussi <leonardobezerrabussy@gmail.com>
2021-02-25 11:38:49 -0300

    finish

---
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Copyright &copy; 2021
+            <a href='https://leonardobussi.github.io' target='_blank'> LeoBussi</a>.
+        </strong>
+    </footer>
+)
