b05cc8082fb59ecf04d5b9c131a9fc7fdf262c99
leonardobussi <leonardobezerrabussy@gmail.com>
2021-02-26 10:31:08 -0300

    add authenticate

---
+++ my-money-app-authenticate/backend/src/config/database.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+// mongodb+srv://rpg:rpg@cluster0.0ddez.mongodb.net/myMoney?retryWrites=true&w=majority
+module.exports = mongoose.connect('mongodb://rpg:rpg@cluster0-shard-00-00.0ddez.mongodb.net:27017,cluster0-shard-00-01.0ddez.mongodb.net:27017,cluster0-shard-00-02.0ddez.mongodb.net:27017/myMoney?ssl=true&replicaSet=atlas-nvmzgw-shard-0&authSource=admin&retryWrites=true&w=majority', {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useFindAndModify: true,
+    useMongoClient: true
+}, ()=> {
+    try {
+        console.log("Mongo conectado")
+      } catch (error) {
+        console.log(`Error: ${error}`)
+      }
+})
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = 
+    "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = 
+    "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = 
+    "'{VALUE}' não é válido para o atributo '{PATH}'."
